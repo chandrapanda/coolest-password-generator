119b8291ab0ead1575c305ed1b0e3762f5d9dcdf
Chandra Holt <chandra_holt@hotmail.com>
2021-11-14 19:08:45 -0800

    if and else if statements added for password options

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,10 @@ number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // User input variables
 
 var enter;
-var pwNumber;
+var pwNumeric;
 var pwCharacter;
 var pwUpperCase;
 var pwLowerCase;
-var pwNumeric;
 var choice;
 
 // Capitalizes letters
@@ -33,9 +32,56 @@ get.addEventListener("click", function () {
 })
 
 // Password generator functions begin here
+// User is prompted about preferred characters
 
 function generatePassword() {
+  enter = parseInt(prompt("Please choose how many characters you'd like your password to be. You may choose any number between 8 and 128."));
+  if (!enter) {
+    alert("Please enter a number between 8 and 128.")
+  
+  } else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("Please enter a number between 8 and 128."));
+  
+  } else {
+    pwNumeric = confirm("Numbers?");
+    pwCharacter = confirm("Special characters?");
+    pwUpperCase = confirm("Upper case letters?");
+    pwLowerCase = confirm("Lower case letters?");
+  };
 
+  // User selections use the four prompts to generate concatenated characters below
+
+  // When no criteria are selected user receives error message
+  
+  if (!pwCharacter && !pwNumeric && !pwUpperCase && pwLowerCase) {
+    choice = alert("Please select desired characters for your password.");
+  }
+
+  // All criteria are selected
+  
+  else if (pwCharacter && pwNumeric && pwUpperCase && pwLowerCase) {
+    choice = character.concat(number, abc, abc2);
+  }
+
+  // Some criteria are selected
+
+  else if (pwCharacter && pwNumeric && pwUpperCase) {
+    choice = character.concat(number, abc2);
+  }
+
+  else if (pwCharacter && pwLowerCase && pwNumeric) {
+    choice = character.concat(abc, number);
+  }
+
+  else if (pwCharacter && pwLowerCase && pwUpperCase) {
+    choice = character.concat(abc, abc2);
+  }
+
+  else if (pwLowerCase && pwUpperCase && pwNumeric) {
+    choice = abc.concat(abc2, number);
+  }
+
+  
   var password = "";
   console.log('generate a password.');
 
